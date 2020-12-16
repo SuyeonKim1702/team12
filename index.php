0f663137e59be3f908552f32a917bdef010ca501
parkjihyun123 <71023004+parkjihyun123@users.noreply.github.com>
2020-12-17 05:59:34 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,54 @@
 <!DOCTYPE html>
 <?php
+
 session_start();
 if(isset($_SESSION[ 'is_logged' ]) && $_SESSION[ 'is_logged' ] == 'Y'){
 //로그인 되었을 경우 
 $top = '<ul class="nav-menu">
-<li><a href="login.html">로그아웃</a></li>
+<li><a href="logout.php">로그아웃</a></li>
 </ul>';
 
 }else{
     $top = '<ul class="nav-menu">
-<li><a href="login.html">로그인</a></li>
-<li><a href="signin.html">회원가입</a></li>
+<li><a href="login.html">Log In</a></li>
+<li><a href="signin.html">Sign Up</a></li>
 </ul>';
 
 }
+
+
+$conn = mysqli_connect(
+    '15.165.124.76',
+    'osp',
+    '1234',
+    'cagong');
+
+
+$cafelist = "SELECT cafeIdx as cafeIdx, x, y, cafename, availableSeat
+from cafe;";
+
+$result = mysqli_query($conn, $cafelist);
+
+$location = array();
+
+while($row1 = mysqli_fetch_assoc($result)){
+ $x = $row1['x'];
+ $y = $row1['y'];
+ $cafeName = $row1['cafename'];
+ $seat = $row1['availableSeat'];
+
+
+ array_push($location, array("x" => $x, "y" => $y, "cafeName" => $cafeName, "seat" => $seat));
+
+ 
+}
+
+
+
+
 ?>
-   
+
+
 <head>
     <meta charset="UTF-8">
     <title>KAGONG</title>
@@ -64,20 +97,21 @@ $top = '<ul class="nav-menu">
                 <nav class="navbar">
                     <div class="nav-logo">
                         <i class="fas fa-coffee"></i>
-                        <a href="index.html">KAGONG</a>
+                        <a href="index.php">KAGONG</a>
                     </div>
                    <?php echo $top ?>
                 </nav>
                 <div class="main-content" >
                     <div class="title">
-                        <h1>카공족을 위한</h1>
-                        <h1>맞춤 카페 추천서비스</h1>
+                        <h1>Cafe Reivew and Recommendation</h1>
+                        <h1>Service for Kagong People</h1>
                     </div>
                 <!--  검색창   -->
                     <form name="searchForm" class = "main-searchbox" action="cafe_list.php" onsubmit="click_search(); return false" method = "POST" >
-                        <div style="display:flex">
-                            <input type="search" placeholder="카페 이름 입력 또는 태그 설정" name="cafeName" id="cafeName" onclick="drop()"/>
+                        <div style="display:flex;">
+                            <input type="search" placeholder="Input cafe name or set tags" name="cafeName" id="cafeName" onclick="drop()"/>
                             <input type="hidden" id="markers" name="markers">
+                            <input type="hidden" id="status" name="status" value="1">
                             <button type = "submit" class = "search-btn"><i class="fas fa-search" style="color:white; font-size:20px;"></i></button>
                         </div>
                         <div id="drop-taglist" style="display:none;">
@@ -132,9 +166,6 @@ $top = '<ul class="nav-menu">
                                         }
                                     }
                                 );
-                        
-
-
                             });
                         </script>
 
@@ -147,22 +178,116 @@ $top = '<ul class="nav-menu">
             <!--    왼쪽 영역     -->
             <div class="bottom-left-container">
                 <div class="intro">
-                    <p>회원님의 현재 위치를 기반으로
-                    <p>주변의 카페 및 업데이트 된
-                    <p>좌석 현황을 볼 수 있습니다.
+                    <p>You can check nearby cafes
+                    <p>and updated seat status
+                    <p>based on your current location
                 </div>
                 <div class="pointerInfo">
                     <ul class="info-list">
-                        <li><i class="fas fa-map-marker-alt" style="color:#FF3300"></i>:거의 만석</li>
-                        <li><i class="fas fa-map-marker-alt" style="color: #FFCC00"></i>:보통</li>
-                        <li><i class="fas fa-map-marker-alt" style="color:#009900"></i>:여유</li>
+                        <li><i class="fas fa-map-marker-alt" style="color:#FF3300"></i>:Almost full</li>
+                        <li><i class="fas fa-map-marker-alt" style="color: #FFCC00"></i>:Usual</li>
+                        <li><i class="fas fa-map-marker-alt" style="color:#009900"></i>:Enough</li>
                     </ul>
                 </div>
             </div>
             <!-- 오른쪽 영역: 지도 -->
             <div class="bottom-right-container">
                 <div id="map" style="width:620px;height:480px;"></div>
-                <script type="text/javascript" src = "homeMap.js"></script>
+                <script type="text/javascript">
+           var a = <?php echo json_encode($location);?>;
+
+
+            var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(37.55708709545054, 126.94558145584872), // 지도의 중심좌표
+        level: 2 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+
+
+
+//마커 좌표위치,좌석정보,남은좌석 수,카페 이름을 데이터베이스에서 받아와서 데이터 배열생성(push);
+var listData= a; 
+
+
+console.log("시행됨");
+
+
+for (var i = 0; i < listData.length; i++) {
+    var position = new kakao.maps.LatLng(listData[i].x, listData[i].y);
+    if (listData[i].seat < 4) {
+        var markerImage = new kakao.maps.MarkerImage('./images/marker_red.png', new kakao.maps.Size(37, 37));
+        var marker = new kakao.maps.Marker({
+            map: map,
+            image: markerImage,
+            position: position
+        });
+        marker.setMap(map);
+
+    } else if (listData[i].seat >= 4 && listData[i].seat <= 10) {
+
+        var markerImage = new kakao.maps.MarkerImage('./images/mark_yellow.png', new kakao.maps.Size(37, 37));
+        var marker = new kakao.maps.Marker({
+            map: map,
+            image: markerImage,
+            position: position
+        });
+        marker.setMap(map);
+    } else {
+        var markerImage = new kakao.maps.MarkerImage('./images/mark_green.png', new kakao.maps.Size(37, 37));
+        var marker = new kakao.maps.Marker({
+            map: map,
+            image: markerImage,
+            position: position
+        });
+        marker.setMap(map);
+    }
+
+    //윈도우 인포에 표시할 내용
+    var content = '<div class="custom">' + listData[i].cafeName +
+        '<div style="color:red; font-size:11px;text-align:left;padding-top:4px">' + ' 남은 좌석 수:' + listData[i].seat + '</div></div>';
+
+    var infowindow = new kakao.maps.InfoWindow({
+        content: content
+    });
+
+// 마커에 이벤트를 등록하는 함수 만들고 즉시 호출하여 클로저를 만듭니다
+    (function (marker, infowindow) {
+        // 마커에 mouseover 이벤트를 등록하고 마우스 오버 시 인포윈도우를 표시
+        kakao.maps.event.addListener(marker, 'mouseover', function () {
+            infowindow.open(map, marker);
+        });
+
+        // 마커에 mouseout 이벤트를 등록하고 마우스 아웃 시 인포윈도우를 닫습니다
+        kakao.maps.event.addListener(marker, 'mouseout', function () {
+            infowindow.close();
+        });
+    })(marker, infowindow);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            
+            
+            
+            </script>
             </div>
         </div>
     </main>
@@ -170,3 +295,19 @@ $top = '<ul class="nav-menu">
 </div>
 </body>
 </html>
+
+
+
+
+
+
+
+
+</html>
+
+
+
+
+
+
+
